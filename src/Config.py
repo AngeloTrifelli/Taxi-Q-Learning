489a4b046a14c315382406493777cd177aeebd65
AngeloTrifelli <trifelli.1920939@studenti.uniroma1.it>
2024-10-14 17:23:23 +0200

    Modified Tabular-Q-Learning chart plot. Adjusted Config parameters

--- src/Config.py
+++ src/Config.py
@@ -4,22 +4,30 @@ tabular_q_learning_props = SimpleNamespace(
     learning_rate = 0.1,
     discount_factor = 0.99,
     exploration_rate = 1.0,
-    exploration_rate_decay = 0.995,
+    exploration_rate_decay = 400,
     minimum_exploration_rate = 0.01,
-    num_episodes = 2000,
+    num_episodes = 3000,
     max_steps_per_episode = 400
 )
 
 deep_q_network_props = SimpleNamespace(
-    learning_rate = 0.001,              #
-    discount_factor = 0.99,             
-    exploration_rate = 1.0,             #Initial exploration rate
-    exploration_rate_decay = 0.995,     #Decay after each selected action
-    minimum_exploration_rate = 0.01,    
-    num_episodes = 2000,                #Total number of episodes that will be executed
-    max_steps_per_episode= 400,         #Maximum number of steps executed for each episode
-    memory_size = 2000,                 #Maximum size of the replay buffer
-    train_start = 1000,                 #Minimum number of elements required in the buffer memory in order to start the training 
-    train_frequency = 10,       
-    batch_size = 64                 
+    network = SimpleNamespace(
+        initial_lr = 0.001,
+        minimum_lr = 0.0001,
+        lr_decay = 5000,
+        batch_size = 128                
+    ),
+    env = SimpleNamespace(
+        num_episodes = 7000,
+        max_steps_per_episode = 100        
+    ),
+    rl = SimpleNamespace(
+        discount_factor = 0.99,
+        exploration_rate = 1,
+        minimum_exploration_rate = 0.1,
+        exploration_rate_decay = 400,
+        memory_size = 5000,
+        train_start = 1000,
+        target_network_update_freq = 20
+    )                    
 )
--- src/Tabular-Q-Learning.py
+++ src/Tabular-Q-Learning.py
@@ -1,6 +1,6 @@
 import gymnasium as gym
 import numpy as np
-import time
+import matplotlib.pyplot as plt
 
 from Config import tabular_q_learning_props
 from tqdm import tqdm
@@ -21,11 +21,13 @@ epsilon_min = tabular_q_learning_props.minimum_exploration_rate
 epsilon_decay = tabular_q_learning_props.exploration_rate_decay
 
 success_table = {}
+total_rewards_list = []
 
 for episode in tqdm(range(num_episodes), desc="Executing"):
     state, info = env.reset() #Start the environment and obtain the initial state    
     completed = False
     current_step = 0
+    total_reward = 0
 
     while not completed and current_step < max_steps:
         action_mask = info.get('action_mask')        
@@ -37,7 +39,8 @@ for episode in tqdm(range(num_episodes), desc="Executing"):
             q_values_valid_actions = q_table[state, valid_actions]
             action = valid_actions[np.argmax(q_values_valid_actions)]  #Exploitation: choose the action with the highest Q-Value
 
-        next_state, reward, terminated, truncated, new_info = env.step(action)     #Execute the action
+        next_state, reward, done, truncated, new_info = env.step(action)     #Execute the action
+        total_reward += reward
 
         #Update the Q-Value
         best_next_action = np.argmax(q_table[next_state, :])
@@ -47,12 +50,13 @@ for episode in tqdm(range(num_episodes), desc="Executing"):
         info = new_info
         current_step += 1
 
-        if terminated or truncated:
+        if done or truncated:
             if not truncated:                
                 success_table[episode] = True
             completed = True
-
-    epsilon = max(epsilon_min, epsilon * epsilon_decay)    
+       
+    epsilon = epsilon_min + (tabular_q_learning_props.exploration_rate - epsilon_min) * np.exp(-episode / epsilon_decay)        
+    total_rewards_list.append(total_reward)
 
 
 print(f"Total number of successfull episodes: {len(success_table)}")
@@ -64,35 +68,48 @@ new_env = gym.make('Taxi-v3', render_mode="human")
 epsilon_eval = 0.3
 
 
-for episode in range(10):
-    print(f"Episode number: {episode + 1}")
-    state, info = new_env.reset()
+# for episode in range(10):
+#     print(f"Episode number: {episode + 1}")
+#     state, info = new_env.reset()
 
-    for _ in tqdm(range(300)):
-        action_mask = info.get('action_mask')
-        valid_actions = np.where(action_mask == 1)[0]
+#     for _ in tqdm(range(300)):
+#         action_mask = info.get('action_mask')
+#         valid_actions = np.where(action_mask == 1)[0]
 
-        if np.random.uniform(0, 1) < epsilon_eval:
-            action = np.random.choice(valid_actions) 
-        else:
-            q_values_valid_actions = q_table[state, valid_actions]
-            action = valid_actions[np.argmax(q_values_valid_actions)]
+#         if np.random.uniform(0, 1) < epsilon_eval:
+#             action = np.random.choice(valid_actions) 
+#         else:
+#             q_values_valid_actions = q_table[state, valid_actions]
+#             action = valid_actions[np.argmax(q_values_valid_actions)]
         
-        next_state, reward, terminated, truncated, new_info = new_env.step(action)   
+#         next_state, reward, terminated, truncated, new_info = new_env.step(action)   
 
-        state = next_state
-        info = new_info
+#         state = next_state
+#         info = new_info
 
-        if terminated or truncated:            
-            if truncated:
-                print(f"Insuccess for episode {episode + 1}")
-            else: 
-                print(f"Success for episode {episode + 1}")
+#         if terminated or truncated:            
+#             if truncated:
+#                 print(f"Insuccess for episode {episode + 1}")
+#             else: 
+#                 print(f"Success for episode {episode + 1}")
 
-            break
+#             break
 
 new_env.close()
         
+
+fig, (ax1) = plt.subplots(1, 1, figsize=(10, 7))        
+plt.title("Training result")
+
+ax1.set_xlabel('Episode')
+ax1.set_ylabel('Rewards')
+ax1.set_ylim(-230, 250)
+ax1.set_title("Rewards")
+ax1.plot(total_rewards_list, color="red")
+
+plt.show()
+
+
 
 
 
