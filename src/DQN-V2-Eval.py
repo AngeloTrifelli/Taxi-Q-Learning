0e7563f1532b179e8b0f3820dc134e97659c6121
AngeloTrifelli <trifelli.1920939@studenti.uniroma1.it>
2024-10-14 20:17:37 +0200

    modified V2 evaluation

--- src/DQN-V2-Eval.py
+++ src/DQN-V2-Eval.py
@@ -12,32 +12,28 @@ class RLModel(nn.Module):
     def __init__(self, input_size, output_size):
         super(RLModel, self).__init__()
 
-        self.embedding = nn.Embedding(input_size, 4)
-        self.fc1 = nn.Linear(4, 25)
-        self.fc2 = nn.Linear(25, 25)
-        self.output = nn.Linear(25, output_size)
+        self.emb = nn.Embedding(input_size, 4)
+        self.l1 = nn.Linear(4, 50)
+        self.l2 = nn.Linear(50, 50)
+        self.l3 = nn.Linear(50, output_size)
 
     def forward(self, x):
-        x = self.embedding(x)
-        x = nn.functional.relu(self.fc1(x))    
-        x = nn.functional.relu(self.fc2(x))
-        x = self.output(x)
+        x = self.emb(x)
+        x = nn.functional.relu(self.l1(x))    
+        x = nn.functional.relu(self.l2(x))
+        x = self.l3(x)
         return x
     
 
 def choose_action(model, state, info):
     action_mask = info.get('action_mask')   
 
-    with torch.no_grad():                       
-        if np.random.uniform(0, 1) < 0.3:
-            valid_actions = np.where(action_mask == 1)[0]
-            return np.random.choice(valid_actions)
-        else:            
-            predicted = model(torch.tensor([state], device=torch.device('cpu')))        
-            q_values = predicted.cpu().numpy()[0]
-            q_values[action_mask == 0] = -float('inf')
-            action = q_values.argmax()        
-            return action 
+    with torch.no_grad():                               
+        predicted = model(torch.tensor([state], device=torch.device('cpu')))        
+        q_values = predicted.cpu().numpy()[0]
+        q_values[action_mask == 0] = -float('inf')
+        action = q_values.argmax()        
+        return action 
 
 
 
@@ -46,9 +42,7 @@ state_size = env.observation_space.n
 action_size = env.action_space.n
 
 model = RLModel(state_size, action_size).to(torch.device('cpu'))
-
-model_path = torch.load(Path('./src/models/DQN-V2.pt'))
-model.load_state_dict(torch.load(model_path)['model_state_dict'])
+model.load_state_dict(torch.load(Path('./src/DQN-V2.pth')))
 model.eval()
 
 
